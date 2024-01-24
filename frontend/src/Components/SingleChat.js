09f085b097b52ff0693291be8895e6ab7229c94e
Izharkhan2022 <izhar.khan@softqubes.com>
2024-01-24 16:26:39 +0530

    update socket path

--- frontend/src/Components/SingleChat.js
+++ frontend/src/Components/SingleChat.js
@@ -21,7 +21,8 @@ import axios from "axios";
 import ScrollableChat from "./ScrollableChat";
 import { io } from "socket.io-client";
 
-const ENDPOINT = "http://localhost:5000";
+const ENDPOINT = "http://127.0.0.1:5000";
+// const ENDPOINT = "http://localhost:5000";
 var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
