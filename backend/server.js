63c59bc0ff87aae3f35db9cdca773898dba35d2a
Izharkhan2022 <izhar.khan@softqubes.com>
2024-01-24 15:15:48 +0530

    updt spelling

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ app.use("/api/message", messageRoutes);
 
 const _dirname1 = path.resolve();
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(_dirname1, "/Frontend/build")));
+  app.use(express.static(path.join(_dirname1, "/frontend/build")));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(_dirname1, "frontend", "build", "index.html"));
   });
